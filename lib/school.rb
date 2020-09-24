a379d7abf5c5a461f52a94ce8ae3e26d3595795e
Alan Young <alanryoung01@gmail.com>
2020-09-24 11:24:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,13 @@
-class School 
+class School
   attr_accessor :name, :roster
 
   def initialize(name)
     @name = name
     @roster - {}
-  end 
+  end
 
   def add_student(student, grade)
     roster[grade] || = []
     roster[grade] << student
-  end 
+  end
   
