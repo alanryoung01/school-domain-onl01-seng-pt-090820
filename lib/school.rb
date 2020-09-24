34e9d6e6c2a4949c7e985f7aa1f14ccaa926b2d7
Alan Young <alanryoung01@gmail.com>
2020-09-24 11:33:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -23,10 +23,9 @@ end
     end
 
 
-    def sort
-      @roster.each do |grade, name|
-        @roster[grade] = name.sort
-      end
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
     end
   end
 end
