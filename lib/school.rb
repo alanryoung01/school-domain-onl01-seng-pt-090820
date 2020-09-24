1e169b72d7e55a9076075fc823f7818d42c042e5
Alan Young <alanryoung01@gmail.com>
2020-09-24 11:34:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,13 @@
 class School
-  def initialize(school_name)
-     @school_name = school_name
+  def initialize(name)
+     @name = name
      @roster = {}
    end
-
+ 
    def roster
      @roster
    end
-
+ 
    def add_student(name, grade)
      @student_name = name
      @grade = grade
@@ -16,11 +16,11 @@ class School
      end
      @roster[grade] << name
    end
-
+ 
    def grade(number)
      @roster[number]
    end
-
+ 
    def sort
      @roster.each do |grade, name|
        @roster[grade] = name.sort
