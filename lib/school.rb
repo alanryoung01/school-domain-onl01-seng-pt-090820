ed07e10b95309f1dc01053c1032da4eb2eb8377b
Alan Young <alanryoung01@gmail.com>
2020-09-24 11:26:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,8 +6,12 @@ class School
     @roster - {}
   end
 
-  def add_student(student, grade)
-    roster[grade] ||= []
-    roster[grade] << student
-  end
-end
+  def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
+end 
+  
+end 
